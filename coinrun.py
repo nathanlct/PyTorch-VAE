6c72ad6602a77b8c3033eb62eef2ef2ce60d52e7
Nathan Lichtlé <nathan.lichtle@gmail.com>
2020-11-30 16:27:51 +0100

    debug

--- coinrun.py
+++ coinrun.py
@@ -19,9 +19,11 @@ class CoinrunDataset(data.Dataset):
         if not self.transform:
             self.transform = lambda x: x
         
-        print(f'Initialized dataset {split} from {filepath}, with shape {self.data.shape}')
+        print(f'Initialized dataset {split} from {filepath} with shape {self.data.shape}')
 
     def __getitem__(self, index):
+        print("OOO", self.transform(self.data[index]), None)
+        print(np.array(self.transform(self.data[index])).shape)
         return self.transform(self.data[index]), None
 
     def __len__(self):
