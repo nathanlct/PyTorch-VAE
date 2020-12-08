b4f1e37fcc21890485583867873819a62fc5c87e
Nathan Lichtlé <nathan.lichtle@gmail.com>
2020-12-08 16:54:47 +0100

    debug logdir

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ from models import *
 from experiment import VAEXperiment
 import torch
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import Callback
 
 # get .yaml config file
 parser = argparse.ArgumentParser(description='Generic runner for VAE models')
@@ -25,6 +26,18 @@ if seed is not None:
     torch.set_deterministic(True)
     torch.backends.cudnn.benchmark = False  # can reduce performance
 
+# setup callbacks
+class MyPrintingCallback(Callback):
+
+    def on_init_start(self, trainer):
+        print('Starting to init trainer!')
+
+    def on_init_end(self, trainer):
+        print('trainer is init now')
+
+    def on_train_end(self, trainer, pl_module):
+        print('do something when training ends')
+
 # create experiment
 model = vae_models[config['model_params']['name']](**config['model_params'])
 experiment = VAEXperiment(model, config['exp_params'])
@@ -32,7 +45,10 @@ experiment = VAEXperiment(model, config['exp_params'])
 # create trainer (https://pytorch-lightning.readthedocs.io/en/stable/trainer.html)
 now = datetime.now().strftime("%d%b%Y-%Hh%Mm%Ss")
 save_dir = config['logging_params']['save_dir'].replace('{date}', now)
-runner = Trainer(default_root_dir=save_dir, **config['trainer_params'])
+runner = Trainer(
+    default_root_dir=save_dir, 
+    callbacks=[MyPrintingCallback()],
+    **config['trainer_params'])
 
 # train
 print(f"======= Training {config['model_params']['name']} =======")
