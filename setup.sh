b287ba4d715fe585284c50c075f4c60adc8044d7
Nathan Lichtlé <nathan.lichtle@gmail.com>
2020-12-18 14:23:17 +0100

    setup

--- setup.sh
+++ setup.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
 
+"""
 git clone https://github.com/nathanlct/PyTorch-VAE.git
 cd PyTorch-VAE/
+"""
 
-conda create --name vae python=3.7 -y
-conda activate vae
-pip install -r requirements.txt
-
-python run.py -c configs/vae.yaml
-python run.py -c configs/bhvae.yaml
-python run.py -c configs/bbvae.yaml
-python run.py -c configs/factorvae.yaml
-
-
-# new
-
-git clone https://github.com/nathanlct/PyTorch-VAE.git
-cd PyTorch-VAE/
 conda create --name vae python=3.7 -y
 conda activate vae
 conda install nb_conda -y
-conda install pytorch-lightning -c conda-forge -y
 pip install torchvision
 pip install torchnet
 pip install torchsummary
 pip install matplotlib
 pip install tensorflow
 pip install tensorboard
-pip install f3fs
+pip install boto3
 export CUBLAS_WORKSPACE_CONFIG=:4096:8  # for deterministicity
 
+sudo apt-get install language-pack-fr
+sudo dpkg-reconfigure locales
+
 # setup aws credentials (upload ~/.aws/credentials)
+
+# python main.py --expname test_overfit --local --epochs 200 --validate_every 10 --checkpoint_every 50 --s3
